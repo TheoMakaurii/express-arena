4d9d9edf6144b66e93f15c0f6031fcefda98748c
Will Zeiher <wzeiher3@gmail.com>
2020-09-14 15:58:55 -0400

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+'use strict';
+
+const e = require('express');
 const express = require('express');
 const app = express();
 const morgan = require('morgan')
@@ -24,6 +27,33 @@ app.get('/cipher', (req, res)=> {
   }
   res.send(text.join(''));
   
+});
+
+app.get('/endpoint', (req, res)=> {
+  const reqArr = req.query.numbers;
+  for(let i = 0; i < reqArr.length; i++){
+    reqArr[i] = parseInt(reqArr[i], 10);
+  }
+
+  const newArr = [];
+  for(let i = 0; i < 6; i++)
+    newArr.push(Math.floor(Math.random() * 20) + 1);
+
+  let counter = 0;
+  for(let i = 0; i < reqArr.length; i++){
+    if(newArr.includes(reqArr[i]))
+      counter++;
+  }
+
+  console.log(counter);
+
+  if(counter < 4)
+    res.send('Sorry, you lose');
+  else if(counter === 5 ) 
+    res.send('Congratulations! You win $100!');
+  else  
+    res.send('Congratulations, you win a free ticket');
+
 });
 
 app.listen(8000, () => {
